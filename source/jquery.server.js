53d977869ec902abb33d93bef9996aefd42b5926
Jason Ramos <jason@stackideas.com>
2012-10-02 16:30:56 +0800

    Added support for resolve/reject & request throttling.

--- source/jquery.server.js
+++ source/jquery.server.js
@@ -11,9 +11,63 @@
  *
  */
 
-var parser = [];
 
-var defaultOptions = {
+(function(){
+
+var self = $.server = function(options) {
+
+	var request = $.Deferred(),
+
+		ajaxOptions = $.extend(true, {}, self.defaultOptions, options, {success: $.noop()});
+
+	self.queue.addDeferred(function(){
+
+		request.xhr = $.ajax(ajaxOptions)
+
+			.done(function(commands){
+
+				if (!$.isArray(commands)) {
+
+					request.rejectWith(request, "Invalid server response.");
+				}
+
+				$.each(commands, function(i, command)
+				{
+					var type = command.type,
+						parser = self.parsers[type] || options[type];
+
+					if ($.isFunction(parser)) {
+
+						parser.apply(request, command.data);
+					}
+				});
+
+				// If server did not resolve this request
+				if (request.state()==="pending") {
+
+					// We'll resolve it ourselves
+					request.resolveWith(request);
+				}
+
+			})
+			.fail(function(xhr, status, response){
+
+				response = response || "Error retrieving data from server."
+
+				request.rejectWith(request, response);
+			});
+
+		// Mark this queue as resolved
+		this.resolve();
+
+	});
+
+	return request;
+}
+
+self.queue = $.Threads({threadLimit: 1, threadDelay: 1200});
+
+self.defaultOptions = {
 	type: 'POST',
 	data: {
 		tmpl: 'component',
@@ -23,60 +77,12 @@ var defaultOptions = {
 	dataType: 'json'
 };
 
-var methods = {
-	initialize: function(o) {
+self.parsers = {
 
-		var options = $.extend(true, {}, defaultOptions, o);
+	script: function(data) {
 
-		if (!options.url || options.url=='')
-			return $.console.error('$.server: Url is invalid.');
-
-		options.success =
-			function(commands, textStatus, jqXHR)
-			{
-				// Restore user-defined success callback
-				jqXHR.success = o.success || function(){};
-				jqXHR.fail    = o.fail || function(){};
-
-				$.each(commands, function(i, command)
-				{
-					var nativeParser = parser[command.type],
-						customParser = o[command.type];
-
-					if ($.isFunction(nativeParser)) {
-
-						nativeParser.apply(this, [command.data, textStatus, jqXHR]);
-
-					} else if ($.isFunction(customParser)) {
-
-						customParser.apply(this, command.data);
-					}
-				});
-			};
-
-		return $.ajax(options);
-	},
-
-	parser: function(type, func)
-	{
-		if ($.isFunction(func))
-			parser[type] = func;
-	},
-
-	removeParser: function(type)
-	{
-		delete parser[type];
-	}
-}
-
-/**
-* Native $.server command parsers
-*/
-parser['script'] =
-	function(data)
-	{
-		if (typeof data[0] == 'string')
-		{
+		// For hardcoded javascript codes
+		if (typeof data[0] == 'string') {
 			try { eval(data[0]) } catch(err) {};
 			return;
 		}
@@ -112,26 +118,18 @@ parser['script'] =
 				chainBroken = true;
 			}
 		});
-	};
+	},
 
-parser['success'] =
-	function(data, textStatus, jqXHR)
-	{
-		jqXHR.success.apply(this, data);
-	};
+	resolve: function() {
 
-parser['fail'] =
-	function(data, textStatus, jqXHR)
-	{
-		jqXHR.fail.apply(this, data);
-	};
+		this.resolveWith(this, arguments);
+	},
 
-$.server = function(method) {
-	if ( methods[method] ) {
-		return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	} else if ( typeof method === 'object' || ! method ) {
-		return methods.initialize.apply( this, arguments );
-	} else {
-		$.error( 'Method ' +  method + ' does not exist on $.server' );
+	reject: function() {
+
+		this.rejectWith(this, arguments);
 	}
 }
+
+})();
+
